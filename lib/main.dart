f5e18f509bfc2f4c1c7dbe7a7cc09510b47dfd87
Imam Wicaksono Suherman <imamwicaksonosuherman@gmail.com>
2024-12-10 06:17:41 +0700

    add env to git ignore and hide api key

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:cipicapachat/screens/welcome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
@@ -13,6 +14,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: App()));
 }
 
--- lib/services/openAI.dart
+++ lib/services/openAI.dart
@@ -1,15 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class OpenAIServices {
   String endpoint =
       'https://api.openai.com/v1/engines/gpt-3.5-turbo/completions';
   Future<String> getChatbotResponse(String userMessage) async {
+    final token = await dotenv.env['API_TOKEN'];
     final response = await http.post(
       Uri.parse(endpoint),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': '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'
-      },
+      headers: {'Content-Type': 'application/json', 'Authorization': token!},
       body: '{"prompt": "$userMessage", "max_tokens": 150}',
     );
     if (response.statusCode == 200) {
